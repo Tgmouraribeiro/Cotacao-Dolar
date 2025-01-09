7a9f95fd48f2e0aec36bd036fbdec9b663b75bcc
unknown <tgmouraribeiro494@gmail.com>
2025-01-09 11:16:26 -0300

    cotacao

--- Cotacao Dolar bs4.py
+++ Cotacao Dolar bs4.py
@@ -2,12 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.google.com/search?q=cotacao+dolar"
+headers = {'User Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'}
 
-requisicao = requests.get(url)
-# print(requisicao.text)
+requisicao = requests.get(url, headers=headers)
+print(requisicao)
 
 site = BeautifulSoup(requisicao.text, 'html.parser')
 
-titulo = site.find('title')
-print(titulo)
+cotacao = site.find('span', class_='hgKElc')
+print(cotacao)
+
 
